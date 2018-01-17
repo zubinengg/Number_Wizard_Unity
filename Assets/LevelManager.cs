4df50736072f01b9c164b03472bb0b7a9cb14b11
Ravi Chhawal <zubin.engg@gmail.com>
2018-01-17 13:11:43 +0530

    test1

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+	public void LoadLevel (string LevelName)
+	{
+		Debug.Log ("Level Load requested for lvl: " + LevelName);
+		Application.LoadLevel(LevelName);
+	}
+	
+	public void QuitLevel ()
+	{
+		Debug.Log("Level quit requested");
+		Application.Quit();
+	}
+}
---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+
+	// Use this for initialization
+	int max ;
+	int min ;
+	int guess ;
+	int maxGuessAllowed = 5;
+	
+	public Text text;
+	
+	void Start ()
+	{
+		StartGame ();	
+	}
+	
+	void StartGame ()
+	{
+		max = 1000;
+		min = 1;
+		guess = Random.Range(min,max);
+		text.text = guess.ToString()	;
+	}
+	
+	void NextGuess ()
+	{
+		guess = Random.Range(min,max);
+		text.text = guess.ToString();
+		maxGuessAllowed -=1;
+		if (maxGuessAllowed<=0)
+		{
+			Application.LoadLevel("Win");
+		}
+	}
+	// Update is called once per frame
+
+	public void GuessLower ()
+	{
+		max = guess;
+		NextGuess ();
+	}
+	
+	public void GuessHigher ()
+	{
+		min = guess;
+		NextGuess ();
+	}
+}
